296b894540c6c547ee6bdea83235d8410eabe033
VanGogh1210 <1669476297@qq.com>
2023-04-23 22:05:10 +0800

    Stack_creat

---
+++ stack- create/stack- create/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define Maxsize 20
+typedef int Elemtype;
+typedef struct {
+    Elemtype data[Maxsize];
+    int top;
+}sqstack;
+
+
+
+
+int main(int argc, const char * argv[]) {
+    
+}
